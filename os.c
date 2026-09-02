0fded15d67cb5ec3daf544ea992092858b6be0d0
Chris Curl <curlc@yahoo.com>
2026-09-02 10:59:33 -0400

    Refactor string and memory manipulation functions

--- os.c
+++ os.c
@@ -6,7 +6,7 @@ void repl() {
     char *tib = (char *)(last-1024);
     if (state != COMPILE) { state = INTERPRET; }
     zType((state == COMPILE) ? " ... "  : " ok\n");
-    push((cell)tib); push(256); outer("accept");
+    outer("last 1024 - 256 accept");
     if (pop()) {
         emit(' ');
         outer(tib);
@@ -28,7 +28,7 @@ int strlen(const char *s) {
 
 char *strcpy(char *dest, const char *src) {
     char *out = dest;
-    while ((*dest++ = *src++) != '\0') { }
+    while ((*(dest++) = *(src++)) != '\0') { }
     return out;
 }
 
@@ -37,8 +37,8 @@ int strEqI(const char *a, const char *b) {
     while (*a && *b) {
         unsigned char ca = (unsigned char)*a;
         unsigned char cb = (unsigned char)*b;
-        if (ca >= 'A' && ca <= 'Z') { ca = (unsigned char)(ca + 32); }
-        if (cb >= 'A' && cb <= 'Z') { cb = (unsigned char)(cb + 32); }
+        if (btwi(ca,'A','Z')) { ca += 32; }
+        if (btwi(cb,'A','Z')) { cb += 32; }
         if (ca != cb) { return 0; }
         ++a; ++b;
     }
@@ -49,7 +49,7 @@ int strEqI(const char *a, const char *b) {
 void *memcpy(void *dest, const void *src, cell num) {
     uint8_t *d = (uint8_t *)dest;
     const uint8_t *s = (const uint8_t *)src;
-    for (cell i = 0; i < num; ++i) { d[i] = s[i]; }
+    for (cell i = 0; i < num; ++i) { *(d++) = *(s++); }
     return dest;
 }
 
@@ -58,9 +58,10 @@ void *memmove(void *dest, const void *src, cell num) {
     const uint8_t *s = (const uint8_t *)src;
     if (d == s || num == 0) { return dest; }
     if (d < s) {
-        for (cell i = 0; i < num; ++i) { d[i] = s[i]; }
+        for (cell i = 0; i < num; ++i) { *(d++) = *(s++); }
     } else {
-        for (cell i = num; i > 0; --i) { d[i - 1] = s[i - 1]; }
+        d += num; s += num;
+        for (cell i = 0; i < num; ++i) { *(--d) = *(--s); }
     }
     return dest;
 }
